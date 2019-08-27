cd9166e67c6058f5e8597230a7290e695c6c3d21
KARTHIK VENUGOPAL <53857484+Karthikvenugopal@users.noreply.github.com>
2019-08-27 17:46:38 +0530

    Add files via upload

---
+++ FACTORIA.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int fact(int n)
+{
+if(n==1)
+return 1;
+return (n*fact(n-1));
+}
+void main()
+{
+int n,a;
+clrscr();
+printf("Enter a number:");
+scanf("%d",&n);
+a = fact(n);
+printf("%d! = %d",n,a);
+getch();
+}
