80252180ed756115c21533535f5e8b7829b56f2b
KARTHIK VENUGOPAL <53857484+Karthikvenugopal@users.noreply.github.com>
2019-08-27 17:51:09 +0530

    Update FACTORIAL.C

--- FACTORIAL.C
+++ FACTORIAL.C
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int fact(int n)
 {
-if(n==1)
-return 1;
-return (n*fact(n-1));
+  if(n==1)
+    return 1;
+  return (n*fact(n-1));
 }
 void main()
 {
-int n,a;
-printf("Enter a number:");
-scanf("%d",&n);
-a = fact(n);
-printf("%d! = %d",n,a);
+  int n,a;
+  printf("Enter a number:");
+  scanf("%d",&n);
+  a = fact(n);
+  printf("%d! = %d",n,a);
 }
